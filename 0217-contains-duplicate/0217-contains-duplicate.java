741a949e242e5fc0ce112e399961de5f617887a3
Aakartion <56947697+Aakartion@users.noreply.github.com>
2023-11-16 00:16:19 -0600

    Time: 11 ms (55.38%), Space: 55.5 MB (46.63%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> mySet = new HashSet<>();
+        for(int num:nums){
+            if(mySet.contains(num)){
+                System.out.println("True");
+                return true;
+            }
+            else{
+                mySet.add(num);
+            }
+        }
+        return false;
+    }
+}
