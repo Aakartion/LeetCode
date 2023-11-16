8dd5c8e286282e227899d4bd0090f2621cd3d818
aakarshan <aakartionsimkhada@gmail.com>
2023-11-16 00:14:26 -0600

    Problem 1 solved

--- Easy/contains_Duplicate/Solution.java
+++ Easy/contains_Duplicate/Solution.java
@@ -1,3 +1,8 @@
+package Easy.contains_Duplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> mySet = new HashSet<>();
