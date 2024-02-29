d8b3cb41af2bb9e0f26c06a61f7d2843f48da641
aakarshan <aakartionsimkhada@gmail.com>
2024-02-28 21:40:35 -0600

    Finding three largest number

---
+++ ThreeLargestNumber/ThreeLargestNumber.java
@@ -0,0 +1,12 @@
+package ThreeLargestNumber;
+
+public class ThreeLargestNumber {
+    public static void main(String[] args) {
+        int[] array = { 7, 20, 18, 4, 20, 19, 20, 24, 3 };
+        getThreeLargestValues(array);
+    }
+
+    private static void getThreeLargestValues(int[] array) {
+
+    }
+}
