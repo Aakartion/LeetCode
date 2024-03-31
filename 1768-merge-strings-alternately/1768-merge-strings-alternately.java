4099e5ae3f71b349d96adb6eaa8c6f75d5b10ac7
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-03-30 22:58:11 -0500

    Time: 1 ms (84.82%), Space: 41.5 MB (70.75%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,34 +1,16 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        if(word1.length()==0){
-            return word2;
-        }
-        else if(word2.length() == 0){
-            return word1;
-        }
-        else{
-            int maxLength = 0;
-            String result = "";
-            if(word1.length()>=word2.length()){
-                maxLength = word1.length();
+        int maxLength = 0;
+        StringBuilder result = new StringBuilder();
+        while(maxLength<word1.length() || maxLength<word2.length()){
+            if(maxLength<word1.length()){
+                result.append(word1.charAt(maxLength));
             }
-            else{
-                maxLength = word2.length();
+            if(maxLength<word2.length()){
+                result.append(word2.charAt(maxLength));
             }
-            for(int i =0; i<maxLength;i++){
-                
-                if(i<word1.length() || i>word1.length()){
-                    if(i<word1.length()){
-                        result = result + word1.charAt(i);
-                    }
-                }
-                if(i<word2.length() || i>word2.length()){
-                    if(i<word2.length()){
-                        result = result + word2.charAt(i);
-                    }
-                }
-            }
-            return result;
+            maxLength ++;
         }
+        return result.toString();
     }
 }
