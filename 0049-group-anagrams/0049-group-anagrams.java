1fc69edb91d5fc83fb565b04da53feda0f605369
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-04-03 22:35:20 -0500

    Time: 6 ms (96.72%), Space: 47.9 MB (37.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -21,3 +21,55 @@ class Solution {
         
 }
 }
+
+// public static List<List<String>> groupAnagram(String[] strs) {
+//         List<List<String>> listOfAnagramStringsList = new ArrayList<>();
+//         if (strs.length == 0) {
+//             return listOfAnagramStringsList;
+//         }
+
+//         for (String str : strs) {
+//             boolean checker = false;
+//             if (listOfAnagramStringsList.isEmpty()) {
+//                 List<String> sList = new ArrayList<>();
+//                 sList.add(str);
+//                 listOfAnagramStringsList.add(sList);
+//             } else {
+//                 for (int i = 0; i < listOfAnagramStringsList.size(); i++) {
+//                     if (isAnagram1(str, listOfAnagramStringsList.get(i).get(0))) {
+//                         listOfAnagramStringsList.get(i).add(str);
+//                         checker = true;
+//                         break;
+//                     }
+//                 }
+//                 if (checker == false) {
+//                     List<String> stringList = new ArrayList<>();
+//                     stringList.add(str);
+//                     listOfAnagramStringsList.add(stringList);
+//                 }
+
+//             }
+//         }
+//         return listOfAnagramStringsList;
+//     }
+
+//     public static Boolean isAnagram1(String s1, String s2) {
+//         if (s1.length() != s2.length()) {
+//             return false;
+//         }
+//         Map<Character, Integer> wordMap = new HashMap<>();
+//         for (int i = 0; i < s1.length(); i++) {
+//             wordMap.put(s1.charAt(i), wordMap.getOrDefault(s1.charAt(i), 0) + 1);
+//         }
+//         for (int i = 0; i < s2.length(); i++) {
+//             wordMap.put(s2.charAt(i), wordMap.getOrDefault(s2.charAt(i), 0) - 1);
+//         }
+//         for (int count : wordMap.values()) {
+//             if (count != 0) {
+//                 return false;
+//             }
+            
+//         }
+//         return true;
+
+//     }
