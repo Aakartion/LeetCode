0fbd79e6686762236a5037a8037e10c152334214
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-04-17 11:00:05 -0500

    Time: 7 ms (64.71%), Space: 47.7 MB (49.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,43 +1,43 @@
-// class Solution {
-//     public List<List<String>> groupAnagrams(String[] strs) {
-//         Map<String,List<String>> anagramMap = new HashMap<>();
-        
-//         for(String str: strs){
-//             char [] charArray = str.toCharArray();
-//             Arrays.sort(charArray);
-//             String sortedString = new String(charArray);
-            
-//             if(!anagramMap.containsKey(sortedString)){
-//                 anagramMap.put(sortedString, new ArrayList<>());
-//             }
-            
-//             anagramMap.get(sortedString).add(str);
-//         }
-//         List<List<String>>result = new ArrayList<>();
-//         for(List<String>list: anagramMap.values()){
-//             result.add(list);
-//         }
-//         return result;
-        
-// }
-// }
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> hashMap = new HashMap<>();
-        for(String str:strs){
+        Map<String,List<String>> anagramMap = new HashMap<>();
+        
+        for(String str: strs){
             char [] charArray = str.toCharArray();
             Arrays.sort(charArray);
             String sortedString = new String(charArray);
-            if(!hashMap.containsKey(sortedString)){
-                hashMap.put(sortedString, new ArrayList<>());
+            
+            if(!anagramMap.containsKey(sortedString)){
+                anagramMap.put(sortedString, new ArrayList<>());
             }
-            hashMap.get(sortedString).add(str);
+            
+            anagramMap.get(sortedString).add(str);
         }
-        List<List<String>> result = new ArrayList<>();
-        for(List<String>strList: hashMap.values()){
-            result.add(strList);
+        List<List<String>>result = new ArrayList<>();
+        for(List<String>list: anagramMap.values()){
+            result.add(list);
         }
         return result;
-    }
+        
 }
+}
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         Map<String, List<String>> hashMap = new HashMap<>();
+//         for(String str:strs){
+//             char [] charArray = str.toCharArray();
+//             Arrays.sort(charArray);
+//             String sortedString = new String(charArray);
+//             if(!hashMap.containsKey(sortedString)){
+//                 hashMap.put(sortedString, new ArrayList<>());
+//             }
+//             hashMap.get(sortedString).add(str);
+//         }
+//         List<List<String>> result = new ArrayList<>();
+//         for(List<String>strList: hashMap.values()){
+//             result.add(strList);
+//         }
+//         return result;
+//     }
+// }
