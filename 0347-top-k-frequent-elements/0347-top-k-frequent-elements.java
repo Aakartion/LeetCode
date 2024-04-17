36a7c6a6910dab1a37c5e063d3fd2dfffe374866
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-04-17 14:46:04 -0500

    Time: 10 ms (96.37%), Space: 48.6 MB (44.14%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,66 +1,40 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-//         Map<Integer, Integer> freqMap = new HashMap<>();
-//         for(int num: nums){
-//             if(!freqMap.containsKey(num)){
-//                 freqMap.put(num, 1);
-//             }
-//             freqMap.put(num, freqMap.get(num)+1);
-//         }
-        
-//         List<Integer> [] bucket = new List[nums.length+1];
-//         for(int num: freqMap.keySet()){
-//             int index = freqMap.get(num);
-//             if(bucket[index]==null){
-//                 bucket[index] = new ArrayList<>();
-//             }
-//             bucket[index].add(num);
-//         }
-        
-//         int [] resultArray = new int [k];
-//         int index = 0;
-//         for(int i = bucket.length -1 ; i >= 0 && index<k; i--){
-//             if(bucket[i]!=null){
-//                 for(int num: bucket[i]){
-//                     resultArray[index] = num;
-//                     index++;
-//                     if(index == k){
-//                         break;
-//                     }
-//                 }
-                
-                
-//             }
-//         }
-//         return resultArray;
-        
-        Map<Integer, Integer> numFrequencyMap = new HashMap<>();
-        for (int num : nums) {
-            numFrequencyMap.put(num, numFrequencyMap.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(int num: nums){
+            // if(!freqMap.containsKey(num)){
+            //     freqMap.put(num, 1);
+            // }
+            // freqMap.put(num, freqMap.get(num)+1);
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-
-        List<Integer>[] buckets = new List[nums.length + 1];
-        for (int num : numFrequencyMap.keySet()) {
-            int frequency = numFrequencyMap.get(num);
-            if (buckets[frequency] == null) {
-                buckets[frequency] = new ArrayList<>();
+        
+        List<Integer> [] bucket = new List[nums.length+1];
+        
+        for(int num: freqMap.keySet()){
+            int freq = freqMap.get(num);
+            if(bucket[freq] == null){
+                bucket[freq] = new ArrayList<>();
             }
-            buckets[frequency].add(num);
+            bucket[freq].add(num);
         }
-
-        int[] result = new int[k];
+        
+        int [] resultArray = new int [k];
         int index = 0;
-        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
-            if (buckets[i] != null) {
-                for (int num : buckets[i]) {
-                    result[index++] = num;
-                    if (index == k) {
+        for(int i = bucket.length -1 ; i >= 0 && index<k; i--){
+            if(bucket[i]!=null){
+                for(int num: bucket[i]){
+                    resultArray[index] = num;
+                    index++;
+                    if(index == k){
                         break;
                     }
                 }
+                
+                
             }
         }
-        return result;
+        return resultArray;
         
     }
 }
