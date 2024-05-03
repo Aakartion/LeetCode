05ae5bd4d9e08df63c894b3e30cb95aedf29acff
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-05-03 11:31:14 -0500

    Time: 24 ms (52.35%), Space: 45 MB (57.63%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -3,27 +3,25 @@ class Solution {
         int left = 0;
         int right = Arrays.stream(piles).max().getAsInt();
         int result = right;
-        for(int i = 1; i< right; i++){
-            while(left<=right){
-                int mid = (left + right)/2;
-                int tempCounter = 0;
-                for(int pile:piles){
-                    tempCounter = tempCounter + (int)Math.ceil((double)pile/mid);
-                    if(tempCounter>h){
-                        left = mid + 1;
-                        break;
-                    }
-                }
-                if(tempCounter <= h){
-                    right = mid - 1;
-                    result = mid;
-                }
-                if(result>tempCounter && tempCounter<=h){
-                    result = mid;
+        while(left<=right){
+            int mid = (left + right)/2;
+            int tempCounter = 0;
+            for(int pile:piles){
+                tempCounter = tempCounter + (int)Math.ceil((double)pile/mid);
+                if(tempCounter>h){
+                    left = mid + 1;
+                    break;
                 }
             }
+            if(tempCounter <= h){
+                right = mid - 1;
+                result = mid;
+            }
+            if(result>tempCounter && tempCounter<=h){
+                result = mid;
+            }
+        }
         
-    }
         return result;
 }
 }
