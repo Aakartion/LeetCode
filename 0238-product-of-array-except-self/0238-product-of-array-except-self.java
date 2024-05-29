1b83e1f7d602c312b465841cd9dd122f6fe761fd
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-05-28 22:36:03 -0500

    Time: 2 ms (90.31%), Space: 55.2 MB (72.53%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
-        int totalProd = 1;
+         int totalProd = 1;
         int zeroCount = 0;
         for(int i = 0; i<nums.length;i++){
             if(nums[i]==0){
@@ -34,6 +33,5 @@ class Solution {
             }
         }
         return result;
-        
     }
 }
