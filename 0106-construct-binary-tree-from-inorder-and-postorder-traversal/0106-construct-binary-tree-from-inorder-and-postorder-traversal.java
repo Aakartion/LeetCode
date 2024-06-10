c33363691a874eea51bd54a0da91961d016072a8
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-06-10 15:01:56 -0500

    Time: 1 ms (97.63%), Space: 43.8 MB (93.28%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int postIndex;
+    Map<Integer, Integer> inorderIndexMap;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        postIndex = postorder.length - 1;
+        inorderIndexMap = new HashMap<>();
+        
+        for(int i = 0; i< inorder.length; i++){
+            inorderIndexMap.put(inorder[i], i);
+        }
+        return buildTreeRecursive(inorder, postorder, 0, inorder.length - 1);
+    }
+    
+    private TreeNode buildTreeRecursive(int []inorder, int []postorder, int inorderStart, int inorderEnd){
+        if(inorderStart > inorderEnd){
+            return null;
+        }
+        int rootVal = postorder[postIndex--];
+        
+        
+        
+        TreeNode root = new TreeNode(rootVal);
+        
+        int inorderIndex = inorderIndexMap.get(rootVal);
+        
+        root.right = buildTreeRecursive(inorder, postorder, inorderIndex + 1, inorderEnd);
+        root.left = buildTreeRecursive(inorder, postorder, inorderStart, inorderIndex - 1);
+        
+        return root;
+    }
+    
+}
