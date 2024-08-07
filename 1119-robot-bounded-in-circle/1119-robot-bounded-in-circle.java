ee933c718a85b12087a3a6a0c381fdd40c5b9c53
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-08-07 15:40:29 -0500

    Time: 0 ms (100%), Space: 41.3 MB (41.95%) - LeetHub

--- 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.java
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean isRobotBounded(String instructions) {
         int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int x = 0, y = 0; // Initial position
-        int dir = 0; // Initial direction (north)
+        int x = 0, y = 0; 
+        int dir = 0;
 
         for (char instruction : instructions.toCharArray()) {
             if (instruction == 'G') {
                 x += directions[dir][0];
                 y += directions[dir][1];
             } else if (instruction == 'L') {
-                dir = (dir + 3) % 4; // Turn left (anti-clockwise)
+                dir = (dir + 3) % 4;
             } else if (instruction == 'R') {
-                dir = (dir + 1) % 4; // Turn right (clockwise)
+                dir = (dir + 1) % 4;
             }
         }
 
-        // After one cycle:
-        // If the robot returns to the origin or is not facing north, it will stay within a bounded area.
         return (x == 0 && y == 0) || dir != 0;
         }
 }
