8c78a54c96837ae72cbb8fb7170fba099427d748
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-08-13 18:31:38 -0500

    Time: 6 ms (81.2%), Space: 45.4 MB (18.16%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,19 +1,38 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Map<Character, Integer> characterMap = new HashMap<>();
-        for(int i = 0; i< s.length();i++){
-            if(!characterMap.containsKey(s.charAt(i))){
-                characterMap.put(s.charAt(i), 1);
-            }else{
-                characterMap.put(s.charAt(i), characterMap.get(s.charAt(i))+1);
-            }
+        // Solution 1:
+        // Map<Character, Integer> characterMap = new HashMap<>();
+        // for(int i = 0; i< s.length();i++){
+        //     if(!characterMap.containsKey(s.charAt(i))){
+        //         characterMap.put(s.charAt(i), 1);
+        //     }else{
+        //         characterMap.put(s.charAt(i), characterMap.get(s.charAt(i))+1);
+        //     }
+        // }
+        // for(int i = 0; i<s.length();i++){
+        //     if(characterMap.get(s.charAt(i))==1){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // Solution 2:
+
+        int[] count = new int[26];  // Array to store frequency of each character
+
+        // First pass: populate the frequency array
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
         }
-        for(int i = 0; i<s.length();i++){
-            if(characterMap.get(s.charAt(i))==1){
+
+        // Second pass: find the first unique character
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
-        System.out.println(characterMap);
+
+        // If no unique character is found
         return -1;
     }
 }
