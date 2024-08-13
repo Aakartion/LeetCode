fc6b59e85ad45ab715950c900e5e6e5071e1bd3f
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-08-13 17:13:58 -0500

    Time: 2 ms (75.55%), Space: 41.9 MB (11.41%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,20 +1,28 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+        // String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
         Set<Character> letters = new HashSet<>();
 
-        for(char c: sentence.toLowerCase().toCharArray()){
-            letters.add(c);
-            // if(ALPHABET.indexOf(c)!= -1){
-            //     letters.add(c);
-            // }
-        }
+        // for(char c: sentence.toLowerCase().toCharArray()){
+        //     if(ALPHABET.indexOf(c)!= -1){
+        //         letters.add(c);
+        //     }
+        // }
 
-        for(char c: ALPHABET.toCharArray()){
-            if(!letters.contains(c)){
-                return false;
+        // for(char c: ALPHABET.toCharArray()){
+        //     if(!letters.contains(c)){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        for(char c: sentence.toCharArray()){
+            letters.add(c);
+            if(letters.size()==26){
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
