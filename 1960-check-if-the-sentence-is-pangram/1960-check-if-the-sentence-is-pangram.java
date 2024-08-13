5a60a094ed0bb2c41fa26c303939ea8d7ac4fb7e
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-08-13 16:59:21 -0500

    Time: 6 ms (8.23%), Space: 41.7 MB (29.98%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+        Set<Character> letters = new HashSet<>();
+
+        for(char c: sentence.toLowerCase().toCharArray()){
+            if(ALPHABET.indexOf(c)!= -1){
+                letters.add(c);
+            }
+        }
+
+        for(char c: ALPHABET.toCharArray()){
+            if(!letters.contains(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
