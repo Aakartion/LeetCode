44eea2eb38685d92e4480f59b3e968092f2752ff
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-08-14 01:59:39 -0500

    Time: 6 ms (81.2%), Space: 45 MB (62.38%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -17,22 +17,15 @@ class Solution {
         // return -1;
 
         // Solution 2:
-
-        int[] count = new int[26];  
-
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i); 
-            int index = currentChar - 'a'; 
-            count[index] = count[index]+ 1;                   
+        int [] letterFreq = new int [26];
+        for(int i = 0; i< s.length();i++){
+            letterFreq[s.charAt(i)-'a']++;
         }
-
-
-        for (int i = 0; i < s.length(); i++) {
-            if (count[s.charAt(i) - 'a'] == 1) {
+        for(int i = 0; i<s.length();i++){
+            if(letterFreq[s.charAt(i) - 'a'] == 1){
                 return i;
             }
         }
-
         return -1;
     }
 }
