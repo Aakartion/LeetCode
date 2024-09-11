614f8a002859b86828ac91a45eff741a38c0430c
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-09-11 13:45:03 -0500

    Time: 8 ms (68.85%), Space: 45 MB (70.27%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,15 +1,48 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Map<Character, Integer> characterFreq = new HashMap<>();
-        for(int i =0; i<s.length();i++){
-            characterFreq.put(s.charAt(i), characterFreq.getOrDefault(s.charAt(i),0)+1);
+        int [] totalLetter = new int [26];
+        for(int i=0;i<s.length();i++){
+            totalLetter[(s.charAt(i)-'a')] = totalLetter[s.charAt(i)-'a']+1;
         }
-        System.out.println(characterFreq);
-        for(int i = 0; i<s.length();i++){
-            if(characterFreq.get(s.charAt(i))==1){
+        for(int i=0; i<s.length();i++){
+            if(totalLetter[s.charAt(i)-'a']==1){
                 return i;
             }
         }
         return -1;
+        
     }
 }
+
+
+
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         Map<Character, Integer> characterFreq = new HashMap<>();
+//         for(int i =0; i<s.length();i++){
+//             characterFreq.put(s.charAt(i), characterFreq.getOrDefault(s.charAt(i),0)+1);
+//         }
+//         System.out.println(characterFreq);
+//         for(int i = 0; i<s.length();i++){
+//             if(characterFreq.get(s.charAt(i))==1){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+       // // Solution 2:
+        // int [] letterFreq = new int [26];
+        // for(int i = 0; i< s.length();i++){
+        //     char currentChar = s.charAt(i); 
+        //     int index = currentChar - 'a'; 
+        //     letterFreq[index] = letterFreq[index]+ 1;  
+        // }
+        // for(int i = 0; i<s.length();i++){
+        //     if(letterFreq[s.charAt(i) - 'a'] == 1){
+        //         return i;
+        //     }
+        // }
+        // return -1;
