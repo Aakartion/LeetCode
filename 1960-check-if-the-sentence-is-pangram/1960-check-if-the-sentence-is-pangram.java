a42c6ca851f73b08eea6e14fe8cea06accf55312
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-09-11 15:10:27 -0500

    Time: 0 ms (100%), Space: 41.6 MB (34.3%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,12 +1,25 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        Set<Character> pangramSet = new HashSet<>();
-        for(int i = 0; i< sentence.length();i++){
-            pangramSet.add(sentence.charAt(i));
+        for(int i=0;i<26;i++){
+            char currCharacter = (char)('a'+i);
+            if(sentence.indexOf(currCharacter)==-1){
+                return false;
+            }
         }
-        if(pangramSet.size()==26){
-            return true;
-        }
-        return false;
+        return true;
     }
 }
+
+
+// class Solution {
+//     public boolean checkIfPangram(String sentence) {
+//         Set<Character> pangramSet = new HashSet<>();
+//         for(int i = 0; i< sentence.length();i++){
+//             pangramSet.add(sentence.charAt(i));
+//         }
+//         if(pangramSet.size()==26){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
