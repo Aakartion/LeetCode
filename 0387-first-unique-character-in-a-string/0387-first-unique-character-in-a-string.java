3566f8ea309b9c44d28bda1d81b7d000773f1d7e
Aakarshan Simkhada <56947697+Aakartion@users.noreply.github.com>
2024-09-11 13:32:13 -0500

    Time: 37 ms (17.36%), Space: 45 MB (84.76%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,30 +1,12 @@
 class Solution {
     public int firstUniqChar(String s) {
-        // Solution 1:
-        // Map<Character, Integer> characterMap = new HashMap<>();
-        // for(int i = 0; i< s.length();i++){
-        //     if(!characterMap.containsKey(s.charAt(i))){
-        //         characterMap.put(s.charAt(i), 1);
-        //     }else{
-        //         characterMap.put(s.charAt(i), characterMap.get(s.charAt(i))+1);
-        //     }
-        // }
-        // for(int i = 0; i<s.length();i++){
-        //     if(characterMap.get(s.charAt(i))==1){
-        //         return i;
-        //     }
-        // }
-        // return -1;
-
-        // Solution 2:
-        int [] letterFreq = new int [26];
-        for(int i = 0; i< s.length();i++){
-            char currentChar = s.charAt(i); 
-            int index = currentChar - 'a'; 
-            letterFreq[index] = letterFreq[index]+ 1;  
+        Map<Character, Integer> characterFreq = new HashMap<>();
+        for(int i =0; i<s.length();i++){
+            characterFreq.put(s.charAt(i), characterFreq.getOrDefault(s.charAt(i),0)+1);
         }
+        System.out.println(characterFreq);
         for(int i = 0; i<s.length();i++){
-            if(letterFreq[s.charAt(i) - 'a'] == 1){
+            if(characterFreq.get(s.charAt(i))==1){
                 return i;
             }
         }
